3dafd3bb88146a61c71d7dd30d62574689e09ee4
Athooh <s.athooh@gmail.com>
2025-02-20 01:53:51 +0300

    Initial commit

---
+++ src/components/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+// src/pages/Home/Home.js
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to Evarest</h1>
+      <p>We build cutting-edge applications for your business.</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/routes/AppRouter.js
@@ -0,0 +1,26 @@
+// src/routes/AppRouter.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from '../Pages/Home/Home';
+import About from '../Pages/About/About';
+import Services from '../Pages/Services/Services';
+import Contact from '../Pages/Contact/Contact';
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
+
+function AppRouter() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default AppRouter;
