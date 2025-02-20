9cc2f4a37a65c7c4c5c41d7a74e0a41ec810c29c
Athooh <s.athooh@gmail.com>
2025-02-20 12:39:35 +0300

    Enhance Home page layout with hero section and update global styles

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,12 +1,21 @@
 // src/pages/Home/Home.js
 import React from 'react';
 import './Home.css';
+import '../../Button/Button.css';
 
 function Home() {
   return (
     <div className="home">
-      <h1>Welcome to Evarest</h1>
-      <p>We build cutting-edge applications for your business.</p>
+      <div className="hero-section">
+        <div className="hero-content">
+          <h1>"Empowering Businesses, Elevating Innovation."</h1>
+          <p>We build cutting-edge applications for your business.</p>
+          <button className='button'>Get Started</button>
+        </div>
+        <div className="hero-image">
+          <img src="./assets/hero.png" alt="hero" />
+        </div>
+      </div>
     </div>
   );
 }
